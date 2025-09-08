7c01498c69e9db17e074ba56c869fed4ba348d02
Carlos <carloscastellano@acidtango.com>
2025-09-08 20:49:20 +0100

    Refactor: Update domainErrorToHttpStatusCode mapping to include INTERNAL_SERVER_ERROR for internal errors

--- src/shared/infrastructure/errors/domainErrorToHttpStatusCode.ts
+++ src/shared/infrastructure/errors/domainErrorToHttpStatusCode.ts
@@ -5,23 +5,11 @@ const HttpStatus = {
   CONFLICT: 409,
   NOT_FOUND: 404,
   UNAUTHORIZED: 401,
+  INTERNAL_SERVER_ERROR: 500,
 } as const
 
 type HttpStatus = (typeof HttpStatus)[keyof typeof HttpStatus]
 
 export const domainErrorToHttpStatusCode: Record<DomainErrorCode, HttpStatus> = {
-  [DomainErrorCode.AGGREGATE_ALREADY_CREATED_ERROR]: HttpStatus.CONFLICT,
-  [DomainErrorCode.INVALID_DATE_RANGE]: HttpStatus.BAD_REQUEST,
-  [DomainErrorCode.MAXIMUM_COSPEAKERS_REACHED]: HttpStatus.BAD_REQUEST,
-  [DomainErrorCode.SPEAKER_DOES_NOT_EXISTS]: HttpStatus.NOT_FOUND,
-  [DomainErrorCode.TALK_ALREADY_BEING_REVIEWED]: HttpStatus.BAD_REQUEST,
-  [DomainErrorCode.TALK_CANNOT_BE_APPROVED]: HttpStatus.BAD_REQUEST,
-  [DomainErrorCode.TALK_DESCRIPTION_TOO_LONG]: HttpStatus.BAD_REQUEST,
-  [DomainErrorCode.TALK_DOES_NOT_EXISTS]: HttpStatus.NOT_FOUND,
-  [DomainErrorCode.TALK_EVENT_NOT_FOUND]: HttpStatus.NOT_FOUND,
-  [DomainErrorCode.TALK_TITLE_TOO_LONG]: HttpStatus.BAD_REQUEST,
-  [DomainErrorCode.UNDERAGE_SPEAKER]: HttpStatus.BAD_REQUEST,
-  [DomainErrorCode.SPEAKER_EMAIL_ALREADY_USED]: HttpStatus.CONFLICT,
-  [DomainErrorCode.INVALID_LOGIN_CREDENTIALS]: HttpStatus.UNAUTHORIZED,
-  [DomainErrorCode.PROFILE_NOT_FILLED]: HttpStatus.BAD_REQUEST,
+  [DomainErrorCode.INTERNAL_ERROR]: HttpStatus.INTERNAL_SERVER_ERROR,
 }
