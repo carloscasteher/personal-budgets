0259772cc9ec6290cbaaec2d5d22af768bcd9ef6
Carlos <carloscastellano@acidtango.com>
2025-09-08 20:49:24 +0100

    Feat: Implement unit tests for CreateBudget use case to ensure budget creation functionality

---
+++ src/budgets/use-cases/CreateBudget.test.ts
@@ -0,0 +1,34 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import type { BudgetsRepository } from '../domain/repositories/BudgetsRepository.ts'
+import { BudgetsRepositoryMemory } from '../infrastructure/repositories/BudgetsRepositoryMemory.ts'
+import { CreateBudget } from './CreateBudget.ts'
+import { Month } from '../domain/models/Month.ts'
+import { UserId } from '../../shared/domain/models/ids/UserId.ts'
+import { UuidGeneratorRandom } from '../../shared/infrastructure/services/uuid-generator/UuidGeneratorRandom.ts'
+import { Year } from '../domain/models/Year.ts'
+
+describe('CreateBudget', () => {
+  let createBudget: CreateBudget
+  let budgetsRepository: BudgetsRepository
+
+  beforeEach(() => {
+    budgetsRepository = BudgetsRepositoryMemory.create()
+    createBudget = new CreateBudget(budgetsRepository)
+  })
+
+  it('should create a new budget', async () => {
+    const userId = UserId.fromPrimitives(UuidGeneratorRandom.generate())
+    const month = Month.fromPrimitives(1)
+    const year = Year.fromPrimitives(2021)
+    await createBudget.execute({
+      userId,
+      month,
+      year,
+    })
+
+    const savedBudget = budgetsRepository.findByUserIdMonthAndYear(userId, month, year)
+
+    expect(savedBudget).toBeDefined()
+  })
+})
