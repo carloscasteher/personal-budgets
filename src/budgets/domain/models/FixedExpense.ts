dd4ba3b51c6a2ab2a9f4affabff3360a7e609f2c
Carlos <carloscastellano@acidtango.com>
2025-09-08 21:12:18 +0100

    Feat: Update FixedExpense model to use FixedExpenseId and add fromPrimitives and toPrimitives methods for improved data handling

--- src/budgets/domain/models/FixedExpense.ts
+++ src/budgets/domain/models/FixedExpense.ts
@@ -1,5 +1,6 @@
 import { ExtensableMoneyMovement } from './ExtensableMoneyMovement.ts'
 import { ExtensableMoneyMovementId } from '../../../shared/domain/models/ids/ExtensableMoneyMovementId.ts'
+import { FixedExpenseId } from '../../../shared/domain/models/ids/FixedExpenseId.ts'
 import type { Primitives } from '../../../shared/domain/models/hex/Primitives.ts'
 import { UuidGeneratorRandom } from '../../../shared/infrastructure/services/uuid-generator/UuidGeneratorRandom.ts'
 
@@ -17,7 +18,7 @@ export class FixedExpense extends ExtensableMoneyMovement {
   private constructor(
     startDate: Date,
     endDate: Date,
-    id: ExtensableMoneyMovementId,
+    id: FixedExpenseId,
     description: string,
     amount: number,
     date: Date
@@ -34,5 +35,31 @@ export class FixedExpense extends ExtensableMoneyMovement {
   }: CreateFixedExpenseParams): FixedExpense {
     const id = ExtensableMoneyMovementId.fromPrimitives(UuidGeneratorRandom.generate())
     return new FixedExpense(startDate, endDate, id, description, amount, date)
+  }
+
+  static fromPrimitives(primitives: FixedExpensePrimitives): FixedExpense {
+    return new FixedExpense(
+      primitives.startDate,
+      primitives.endDate,
+      FixedExpenseId.fromPrimitives(primitives.id),
+      primitives.description,
+      primitives.amount,
+      primitives.date
+    )
+  }
+
+  toPrimitives() {
+    return {
+      id: this.id.toPrimitives(),
+      startDate: this.startDate,
+      endDate: this.endDate,
+      description: this.description,
+      amount: this.amount,
+      date: this.date,
+    }
+  }
+
+  getId() {
+    return this.id
   }
 }
